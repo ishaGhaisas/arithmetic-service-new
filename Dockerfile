83780e425edb2fd635d2c0b926a37e9286f6046d
Isha Ghaisas <ighaisas@iu.edu>
2024-02-18 23:06:30 -0500

    updated a few files

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,8 @@
-# Use an official Node.js runtime as a parent image
-FROM node:20.11.0
+FROM node:20.11.0-alpine
 
-# Set the working directory in the container
 WORKDIR /usr/src/app
-
-# Copy package.json and package-lock.json to the working directory
 COPY package*.json ./
+RUN npm ci --omit=dev
+COPY ./src ./src
 
-# Install Node.js dependencies
-RUN npm ci
-# Copy the rest of the application code to the working directory
-COPY . .
-
-# Command to run the Node.js application
 CMD npm start
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const { add } = require("../arithmetica")
+app.use(cors())
+
+const port = 3002;
+
+app.get('/', (req, res) => {
+    res.send('Arithmetic Service - Hello World ');
+});
+// app.get('/add/:n/:m', (req, res) => {
+
+//     res.json(Number(req.params.n) + Number(req.params.m));
+// });
+
+app.get('/add/:n/:m', (req, res) => {
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let sum  = add(n,m);
+        res.json(sum);
+});
+
+app.get('/sub/:n/:m', (req, res) => {
+    res.json(Number(req.params.n) - Number(req.params.m));
+});
+
+app.listen(port);
+
+
+
