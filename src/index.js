10f4460698291bb71878f58c14d952ceb6d186ae
Isha Ghaisas <61582002+ishaGhaisas@users.noreply.github.com>
2024-02-18 23:54:09 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,25 +7,37 @@ app.use(cors())
 const port = 3002;
 
 app.get('/', (req, res) => {
-    res.send('Arithmetic Service - Hello World ');
+    res.send('Arithmetic Service - latest ');
 });
-// app.get('/add/:n/:m', (req, res) => {
 
-//     res.json(Number(req.params.n) + Number(req.params.m));
-// });
 
 app.get('/add/:n/:m', (req, res) => {
     let n = Number(req.params.n);
     let m = Number(req.params.m);
     let sum  = add(n,m);
-        res.json(sum);
+    res.json(sum);
 });
 
 app.get('/sub/:n/:m', (req, res) => {
     res.json(Number(req.params.n) - Number(req.params.m));
 });
 
+app.get('/mul/:n/:m', (req,res) => {
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let mul = n*m;
+    res.json(mul);
+});
+
+app.get('/div/:n/:m', (req,res) => {
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let div = n/m;
+    res.json(div);
+});
+
 app.listen(port);
+
 
 
 
